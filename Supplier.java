1ee021e6fd708198e96535f33e967a93786dcc05
“Jordan <jtdroberts@hotmail.com>
2025-04-30 19:17:50 +0100

    Initial commit

---
+++ Supplier.java
@@ -0,0 +1,35 @@
+public class Supplier {
+    private int id;
+    private String name;
+    private String contact;
+    // private String order_history;
+
+    public Supplier(int id, String name, String contact) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        // Why do I need a constructor? What is this.?
+        // Add order 
+        // update_info()
+
+     }
+    
+    public int getId() {
+        return id;
+        }
+ 
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    // Getters and setters for name
+    
+    public String getName(){
+        return name;
+    }     
+    public void setName(String name){
+        this.name = name;
+    }
+
+        public Supplier(){}
+}
---
+++ main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Supplier Ebay = new Supplier(); 
+        Ebay.setName("Jordan");
+        Ebay.getName();
+        System.out.println(Ebay.getName());
+
+        Ebay.setId(1);
+        Ebay.getId();
+        System.out.println(Ebay.getId());
+    }
+}
+
